26acbb8e895119508e5150b6fcd70b3dddd52862
Erik <erikandronico@gmail.com>
2018-11-12 16:00:26 +0100

    gigi er meglio

--- code/it/marconicivitavecchia/as2018_19/c5Binf/verifica/Main.java
+++ code/it/marconicivitavecchia/as2018_19/c5Binf/verifica/Main.java
@@ -1,27 +1,45 @@
 
 // TODO Inserire il package corretto
 // nota: potete usare il suggerimento della lampadina a sinistra (scegliete l'unica opzione sensata). 
-package __TODO__;
+package Compiti;
 
 import javax.swing.*;
 
 public class Main extends JFrame {
 	
 	public Main() {
-		super("Formula 1 Passion");
+		super("Formula 1 DEDDE");
 		super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
 		// TODO Inserire la size in accordo con il wireframe (valori approssimati)
-		super.setSize(__TODO__,__TODO__);
+		super.setSize(320,240);
 		
 		// TODO Usare StringBuilder per costruire la stringa HTML
-		
+		StringBuilder sb = new StringBuilder();
 		// TODO Creare la stringa HTML per il wireframe
+		sb.append("<html>");
+		sb.append("<head></head>");
+		sb.append("<body>");
+		sb.append("<h1>Formula 1 DEDDE</h1>");
+		sb.append("<table>");
+		sb.append("<tr><th><h3>Pilota</h3></th>");
+		sb.append("<th><h3>Punteggio</h3></th></tr>");
+		sb.append("<tr><th>Pierluigi</th>");
+		sb.append("<th>0</th></tr>");
+		sb.append("<tr><th>Salvatore</th>");
+		sb.append("<th>-200</th></tr>");
+		sb.append("<tr><th>Carmine</th>");
+		sb.append("<th>-400</th></tr>");
+		sb.append("</body>");
+		sb.append("</html>");
 		
+		
+
 		// TODO Convertire lo StringBuilder in String
+		String html= sb.toString();
 		
 		// TODO Assegnare correttamente la stringa create a JLabel
-		super.add(new JLabel(__TODO__));
+		super.add(new JLabel(html));
 		super.setVisible(true);
 		 
 		System.out.println("Verifica Swing-GitHub");
